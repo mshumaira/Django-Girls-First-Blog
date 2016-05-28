513b089abd2c0f49c18419c8152348291d3732a2
humaira.nu@gmail.com <humaira.nu@gmail.com>
2016-05-28 13:24:14 +0500

    My Django Girls app, first commit

---
+++ TestFile/test.py
@@ -0,0 +1,47 @@
+
+
+if (3>5):
+   print("yes true")
+else:
+   print("No false")
+
+
+
+print("hello") 
+print(" world") 
+
+
+Letters=['a','b','m','n','s','h','z','y']
+
+for letter in Letters:
+  print("letter: "+ letter)
+
+
+print("bye")
+
+
+participant={'AB':1,'BB':2,'CB':3,'DB':4}
+
+print(participant.keys())
+print(participant.values())
+print ('AB' in participant.keys())
+
+print(participant['AB'])
+
+
+
+#comments
+
+#sdfsf
+
+""" long comments sad  asd"""
+def hi(name):
+   if(name=='Ola'):
+   		print("Hello Ola")
+   elif(name=='Monica'):
+   		print("Hello Monica")
+   else:
+    	print("not allowed")
+
+
+hi("Monica")
